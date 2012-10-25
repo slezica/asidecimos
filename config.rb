3094709e464ebb67a92e162fc485b27230b70e73
Santiago Lezica <slezica89@gmail.com>
2012-10-24 21:45:59 -0300

    Post github

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/asidecimos/"
+http_images_path = "/asidecimos/images"
+http_fonts_path = "/asidecimos/fonts"
+css_dir = "public/asidecimos/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
